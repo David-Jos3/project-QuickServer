f80af016d9ef494bc0d72c6bd36cb3020d6183cb
David-jos3 <davidgomesgc230@gmail.com>
2025-04-09 19:34:01 -0300

    refactor: rename save method to create in OrdersItemsRepository interface

--- src/repositories/orders-items-repository.ts
+++ src/repositories/orders-items-repository.ts
@@ -1,5 +1,5 @@
 import type { OrderItem, Prisma } from "@prisma/client";
 
 export interface OrdersItemsRepository {
-  save(data: Prisma.OrderItemCreateManyInput): Promise<OrderItem>;
+  create(data: Prisma.OrderItemCreateManyInput): Promise<OrderItem>;
 }
