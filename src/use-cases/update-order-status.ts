4bbff807c1d5d6333e19ebd735541184fc97af00
David-jos3 <davidgomesgc230@gmail.com>
2025-04-09 19:48:04 -0300

    feat: creating use case to update order status

---
+++ src/use-cases/update-order-status.ts
@@ -0,0 +1,66 @@
+// biome-ignore lint/style/useImportType: <explanation>
+import { Orders, OrderStatus, UserRoles } from "@prisma/client";
+import type { OrdersRepository } from "../repositories/orders-repository";
+
+interface UpdateOrderStatusUseCaseRequest {
+  orderId: string;
+  status: OrderStatus;
+  userRole: UserRoles;
+}
+
+interface UpdateOrderStatusUseCaseResponse {
+  order: Orders;
+}
+
+export class UpdateOrderStatusUseCase {
+  constructor(private ordersRepository: OrdersRepository) {}
+
+  async execute({
+    orderId,
+    status,
+    userRole,
+  }: UpdateOrderStatusUseCaseRequest): Promise<UpdateOrderStatusUseCaseResponse> {
+    const orderExists = await this.ordersRepository.findById(orderId);
+
+    if (!orderExists) {
+      throw new Error("Pedido não encontrado");
+    }
+
+    if (
+      (orderExists.status === OrderStatus.PENDING ||
+        orderExists.status === OrderStatus.IN_PROGRESS) &&
+      userRole !== UserRoles.Cozinha
+    ) {
+      throw new Error(
+        "Apenas o usuário da cozinha pode alterar o status do pedido"
+      );
+    }
+
+    if (
+      orderExists.status === OrderStatus.READY &&
+      userRole !== UserRoles.Caixa
+    ) {
+      throw new Error(
+        "Apenas o usuário do caixa pode alterar o status do pedido"
+      );
+    }
+
+    const allowedTransitions: Record<OrderStatus, OrderStatus> = {
+      PENDING: OrderStatus.IN_PROGRESS,
+      IN_PROGRESS: OrderStatus.READY,
+      READY: OrderStatus.COMPLETED,
+      COMPLETED: OrderStatus.COMPLETED,
+    };
+    const allowedStatus = allowedTransitions[orderExists.status];
+
+    if (allowedStatus !== status) {
+      throw new Error(
+        `Status inválido. O status atual é ${orderExists.status} e o próximo status permitido é ${allowedStatus}`
+      );
+    }
+
+    const order = await this.ordersRepository.updateStatus(orderId, status);
+
+    return { order };
+  }
+}
