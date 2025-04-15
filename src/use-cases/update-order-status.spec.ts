eade410e68d98d725db1fc67169c00586c41da96
David-jos3 <davidgomesgc230@gmail.com>
2025-04-14 22:42:16 -0300

    test: ensure correct behavior when updating order status

---
+++ src/use-cases/update-order-status.spec.ts
@@ -0,0 +1,115 @@
+import { InMemoryOrdersRepository } from "../../test/repositories/in-memory-orders-repository";
+import { CreateOrdersUseCase } from "./create-orders";
+import { UpdateOrderStatusUseCase } from "./update-order-status";
+
+let inMemoryOrdersRepository: InMemoryOrdersRepository;
+let updateOrderStatusUseCase: UpdateOrderStatusUseCase;
+let createOrdersUseCase: CreateOrdersUseCase;
+
+describe("Update Order Status Use Case", () => {
+  beforeEach(() => {
+    inMemoryOrdersRepository = new InMemoryOrdersRepository();
+    updateOrderStatusUseCase = new UpdateOrderStatusUseCase(
+      inMemoryOrdersRepository
+    );
+    createOrdersUseCase = new CreateOrdersUseCase(inMemoryOrdersRepository);
+  });
+
+  test("should update the order status successfully", async () => {
+    const { order } = await createOrdersUseCase.execute({
+      cashierId: "fake-cashier-id",
+      customerName: "John Doe",
+    });
+
+    const orderStatus = await updateOrderStatusUseCase.execute({
+      orderId: order.id,
+      status: "IN_PROGRESS",
+      userRole: "Cozinha",
+    });
+    expect(orderStatus.order).toMatchObject({
+      id: order.id,
+      status: "IN_PROGRESS",
+    });
+  });
+
+  test("should throw an error if the order does not exist", async () => {
+    await expect(
+      updateOrderStatusUseCase.execute({
+        orderId: "order-fake-id",
+        status: "IN_PROGRESS",
+        userRole: "Cozinha",
+      })
+    ).rejects.toThrow(new Error("Pedido não encontrado"));
+  });
+
+  test("should throw an error if a non-Kitchen user tries to update the order to 'IN_PROGRESS' or 'READY'", async () => {
+    const { order } = await createOrdersUseCase.execute({
+      cashierId: "fake-cashier-id",
+      customerName: "John Doe",
+    });
+
+    await expect(
+      updateOrderStatusUseCase.execute({
+        orderId: order.id,
+        status: "IN_PROGRESS",
+        userRole: "Caixa",
+      })
+    ).rejects.toThrow(
+      new Error("Apenas o usuário da cozinha pode alterar o status do pedido")
+    );
+  });
+
+  test("should throw an error if a non-Cashier user tries to update the order to 'COMPLETED'", async () => {
+    const { order } = await createOrdersUseCase.execute({
+      cashierId: "fake-cashier-id",
+      customerName: "John Doe",
+    });
+
+    await updateOrderStatusUseCase.execute({
+      orderId: order.id,
+      status: "IN_PROGRESS",
+      userRole: "Cozinha",
+    });
+
+    await updateOrderStatusUseCase.execute({
+      orderId: order.id,
+      status: "READY",
+      userRole: "Cozinha",
+    });
+
+    await expect(
+      updateOrderStatusUseCase.execute({
+        orderId: order.id,
+        status: "COMPLETED",
+        userRole: "Cozinha",
+      })
+    ).rejects.toThrow(
+      new Error("Apenas o usuário do caixa pode alterar o status do pedido")
+    );
+  });
+
+  test("should throw an error when a Kitchen user tries to complete an order", async () => {
+    const { order } = await createOrdersUseCase.execute({
+      cashierId: "fake-cashier-id",
+      customerName: "John Doe",
+    });
+
+    await updateOrderStatusUseCase.execute({
+      orderId: order.id,
+      status: "IN_PROGRESS",
+      userRole: "Cozinha",
+    });
+
+    await expect(
+      updateOrderStatusUseCase.execute({
+        orderId: order.id,
+        status: "COMPLETED",
+        userRole: "Cozinha",
+      })
+    ).rejects.toThrow(
+      new Error(
+        "Status inválido, não foi  possível atualizar o status do pedido"
+      )
+    );
+  });
+});
