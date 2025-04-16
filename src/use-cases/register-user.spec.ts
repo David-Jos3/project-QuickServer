6fcf6c1d6774832ed80f6430f2f9bde3ec22758a
David-jos3 <davidgomesgc230@gmail.com>
2025-04-15 21:11:16 -0300

    test: creating test for user registration

---
+++ src/use-cases/register-user.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryUserRepository } from "../../test/repositories/in-memory-user-repository";
+import { RegisterUserUseCase } from "./register-user";
+
+let inMemoryUserRepository: InMemoryUserRepository;
+let registerUserUseCase: RegisterUserUseCase;
+
+describe("Create user", () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    registerUserUseCase = new RegisterUserUseCase(inMemoryUserRepository);
+  });
+
+  test("Should be able to create new users", async () => {
+    const { user } = await registerUserUseCase.execute({
+      email: "davidgomes123@gmail.com",
+      name: "David José",
+      password: "1234567",
+      role: "Cozinha",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+});
