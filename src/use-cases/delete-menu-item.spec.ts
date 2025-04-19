a03aa13187a6a360be36b2336d2e9ac13c5c0ff6
David-jos3 <davidgomesgc230@gmail.com>
2025-04-18 23:20:45 -0300

    test: ensure menu item can be deleted

---
+++ src/use-cases/delete-menu-item.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryMenuItemRepository } from "./../../test/repositories/in-memory-menu-item";
+import { CreateMenuItemsUseCase } from "./create-menu-item";
+import { DeleteMenuItemUseCase } from "./delete-menu-item";
+
+let inMemoryMenuItemRepository: InMemoryMenuItemRepository;
+let deleteMenuItemUseCase: DeleteMenuItemUseCase;
+let createMenuItemUseCase: CreateMenuItemsUseCase;
+
+describe("Delete Menu Item Use Case", () => {
+  beforeEach(() => {
+    inMemoryMenuItemRepository = new InMemoryMenuItemRepository();
+    deleteMenuItemUseCase = new DeleteMenuItemUseCase(
+      inMemoryMenuItemRepository
+    );
+    createMenuItemUseCase = new CreateMenuItemsUseCase(
+      inMemoryMenuItemRepository
+    );
+  });
+
+  test("must be able to delete a menu item", async () => {
+    const { menuItem } = await createMenuItemUseCase.execute({
+      name: "Pizza (Fatia)",
+      description: "KIT KAT",
+      price: 8.0,
+    });
+
+    await deleteMenuItemUseCase.execute({
+      menuId: menuItem.id,
+    });
+
+    const deletedItem = inMemoryMenuItemRepository.findById(menuItem.id);
+
+    expect(deletedItem).toBeNull();
+  });
+});
