f390b22030d0a7cc8bc2ebe64fd76fe584c52fee
David-jos3 <davidgomesgc230@gmail.com>
2025-04-21 19:05:10 -0300

    feat: add user role to registration payload

--- src/http/controllers/users/register-user.controller.ts
+++ src/http/controllers/users/register-user.controller.ts
@@ -2,30 +2,36 @@ import type { Request, Response } from "express";
 import { z, ZodError } from "zod";
 import { PrismaUserRepository } from "../../../repositories/prisma/prisma-user-repository";
 import { RegisterUserUseCase } from "../../../use-cases/register-user";
+import { UserRoles } from "@prisma/client";
 
 const userBodySchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string(),
+  userRoles: z.nativeEnum(UserRoles),
 });
 
 export class RegisterUserController {
   async register(request: Request, response: Response) {
-    const { email, name, password } = userBodySchema.parse(request.body);
+    const { email, name, password, userRoles } = userBodySchema.parse(
+      request.body
+    );
 
     try {
       const prismaUserRepository = new PrismaUserRepository();
       const registerUserUseCase = new RegisterUserUseCase(prismaUserRepository);
 
-      await registerUserUseCase.execute({ email, name, password });
+      await registerUserUseCase.execute({ email, name, password, userRoles });
 
       response.send({ message: "User created successfully" }).status(201);
     } catch (error) {
       if (error instanceof ZodError) {
-        response.send({ message: error.message }).status(400);
+        response.status(400).send({ message: error.errors });
       }
-
-      response.send({ message: error }).status(500);
+      response.status(500).send({
+        message: "Internal server error",
+        error: error,
+      });
     }
   }
 }
