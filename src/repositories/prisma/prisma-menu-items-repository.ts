af7ca62e18cea02d97155b3da1c50cfa0d274de5
David-jos3 <davidgomesgc230@gmail.com>
2025-04-21 19:07:20 -0300

    feat: expand PrismaMenuItemsRepository with findAll, findById, update, and delete methods

--- src/repositories/prisma/prisma-menu-items-repository.ts
+++ src/repositories/prisma/prisma-menu-items-repository.ts
@@ -1,12 +1,50 @@
-import type { MenuItemsRepository } from "../menu-items-repository"
-import { prisma } from "../../lib/prisma-client"
-import type { MenuItem, Prisma } from "@prisma/client"
+import type { MenuItemsRepository } from "../menu-items-repository";
+import { prisma } from "../../lib/prisma-client";
+import type { MenuItem, Prisma } from "@prisma/client";
 
 export class PrismaMenuItemsRepository implements MenuItemsRepository {
+  async findAll(): Promise<MenuItem[]> {
+    const menuItems = await prisma.menuItem.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        OrderItem: true,
+      },
+    });
+
+    return menuItems;
+  }
+  async findById(menuId: string): Promise<MenuItem | null> {
+    const menuItem = await prisma.menuItem.findUnique({
+      where: { id: menuId },
+    });
+
+    if (!menuItem) {
+      return null;
+    }
+    return menuItem;
+  }
+  async update(menuItem: MenuItem): Promise<MenuItem> {
+    const { id, ...data } = menuItem;
+
+    return await prisma.menuItem.update({
+      where: { id },
+      data: {
+        ...data,
+      },
+    });
+  }
+
+  async delete(menuId: string): Promise<void> {
+    await prisma.menuItem.delete({
+      where: { id: menuId },
+    });
+  }
 
   async create(data: MenuItem): Promise<MenuItem> {
-   return await prisma.menuItem.create({
+    return await prisma.menuItem.create({
       data,
-    })
+    });
   }
 }
