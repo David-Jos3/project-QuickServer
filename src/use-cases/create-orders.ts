604ae682897035c2a065bcc7c1684ac629b2a727
David-jos3 <davidgomesgc230@gmail.com>
2025-04-21 19:10:25 -0300

    fix: remove unused status and set default to PENDING

--- src/use-cases/create-orders.ts
+++ src/use-cases/create-orders.ts
@@ -1,26 +1,26 @@
-import type { OrdersRepository } from '../repositories/orders-repository';
-import type { Orders, OrderStatus } from '@prisma/client';
-
+import type { OrdersRepository } from "../repositories/orders-repository";
+import { type Orders, OrderStatus } from "@prisma/client";
 
 interface CreateOrderUseCaseRequest {
   cashierId: string;
   customerName: string;
-  status: OrderStatus;
 }
 
 interface CreateOrderResponse {
   order: Orders;
 }
 
-
 export class CreateOrdersUseCase {
   constructor(private ordersRepository: OrdersRepository) {}
 
-  async execute({ cashierId, customerName, status }: CreateOrderUseCaseRequest): Promise<CreateOrderResponse> {
-    const order = await this.ordersRepository.save({
-     cashierId,
-     customerName,
-     status,
+  async execute({
+    cashierId,
+    customerName,
+  }: CreateOrderUseCaseRequest): Promise<CreateOrderResponse> {
+    const order = await this.ordersRepository.create({
+      cashierId,
+      customerName,
+      status: OrderStatus.PENDING,
     });
 
     return { order };
