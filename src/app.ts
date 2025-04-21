cc289a26991489a93cdfdaec547c9ecd3835f73f
David-jos3 <davidgomesgc230@gmail.com>
2025-04-21 19:00:53 -0300

    feat: add routes for users, menu items, orders and order items

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import { userRouter } from "./http/controllers/users/router";
+import { menuItemRouter } from "./http/controllers/menu/router";
+import { ordersRouter } from "./http/controllers/orders/router";
+import { ordersItemsRouter } from "./http/controllers/orders-items/router";
 
 export const app = express();
 
 app.use(express.json());
+userRouter(app);
+menuItemRouter(app);
+ordersRouter(app);
+ordersItemsRouter(app);
 
 app.get("/sobre", (req, res) => {
-	res.send({ message: "Sistema de Pedidos Missão Lancai as Redes" });
+  res.send({ message: "Sistema de Pedidos Missão Lancai as Redes" });
 });
