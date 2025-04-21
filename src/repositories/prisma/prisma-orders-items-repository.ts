2da3f43730399336f040ede34b5f29e3a22822af
David-jos3 <davidgomesgc230@gmail.com>
2025-04-21 19:07:58 -0300

    refactor: reorganize PrismaOrdersItemsRepository methods

--- src/repositories/prisma/prisma-orders-items-repository.ts
+++ src/repositories/prisma/prisma-orders-items-repository.ts
@@ -1,11 +1,11 @@
-import type { OrdersItemsRepository } from "../orders-items-repository"
-import type { OrderItem } from "@prisma/client"
-import { prisma } from "../../lib/prisma-client"
+import type { OrdersItemsRepository } from "../orders-items-repository";
+import type { OrderItem } from "@prisma/client";
+import { prisma } from "../../lib/prisma-client";
 
 export class PrismaOrdersItemsRepository implements OrdersItemsRepository {
   async create(data: OrderItem): Promise<OrderItem> {
     return await prisma.orderItem.create({
-      data
-    })
+      data,
+    });
   }
 }
