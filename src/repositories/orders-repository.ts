97fab2489a399c9d4989d03e0e0659e42871ddf1
David-jos3 <davidgomesgc230@gmail.com>
2025-04-21 19:06:31 -0300

    feat: expand orders repository with findAll method

--- src/repositories/orders-repository.ts
+++ src/repositories/orders-repository.ts
@@ -2,6 +2,7 @@ import type { Orders, OrderStatus, Prisma } from "@prisma/client";
 
 export interface OrdersRepository {
   create(data: Prisma.OrdersCreateManyInput): Promise<Orders>;
-  updateStatus(orderId: string, status: OrderStatus): Promise<Orders>;
+  findAll(): Promise<Orders[]>;
+  updatedStatus(orderId: string, status: OrderStatus): Promise<Orders>;
   findById(id: string): Promise<Orders | null>;
 }
