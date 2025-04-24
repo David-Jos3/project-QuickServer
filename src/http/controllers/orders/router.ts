bd3f15bf4aa557440a22e8757294d745175c87e6
David-jos3 <davidgomesgc230@gmail.com>
2025-04-23 21:10:18 -0300

    feat: implement PATCH /orders/status to update order status

--- src/http/controllers/orders/router.ts
+++ src/http/controllers/orders/router.ts
@@ -1,10 +1,18 @@
 import type { Application } from "express";
 import { CreateOrdersController } from "./create-orders.controller";
+import { UpdateOrdersStatusController } from "./update-orders-status.controller";
 
 export function ordersRouter(app: Application) {
   const createOrdersController = new CreateOrdersController();
+  const updateOrdersStatusController = new UpdateOrdersStatusController();
+
   app.post(
     "/orders",
     createOrdersController.create.bind(createOrdersController)
+  );
+
+  app.patch(
+    "/orders/status",
+    updateOrdersStatusController.update.bind(updateOrdersStatusController)
   );
 }
