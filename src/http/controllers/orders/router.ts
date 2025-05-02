3c2bef657ff5f1e3704acc0011556af5b3a5d4d5
David-jos3 <davidgomesgc230@gmail.com>
2025-05-01 21:31:38 -0300

    feat(router): implement GET /orders and GET /orders/:orderId routes with corresponding controllers

--- src/http/controllers/orders/router.ts
+++ src/http/controllers/orders/router.ts
@@ -1,18 +1,29 @@
+import { GetOrderByIdController } from "./get-order-by-id.controller";
 import type { Application } from "express";
 import { CreateOrdersController } from "./create-orders.controller";
 import { UpdateOrdersStatusController } from "./update-orders-status.controller";
+import { GetOrdersController } from "./get-orders.controller";
 
-export function ordersRouter(app: Application) {
+export async function ordersRouter(app: Application) {
   const createOrdersController = new CreateOrdersController();
   const updateOrdersStatusController = new UpdateOrdersStatusController();
+  const getOrdersController = new GetOrdersController();
+  const getOrderByIdController = new GetOrderByIdController();
 
   app.post(
     "/orders",
-    createOrdersController.create.bind(createOrdersController)
+    createOrdersController.handle.bind(createOrdersController)
   );
 
   app.patch(
-    "/orders/status",
-    updateOrdersStatusController.update.bind(updateOrdersStatusController)
+    "/orders/status/:orderId",
+    updateOrdersStatusController.handle.bind(updateOrdersStatusController)
+  );
+
+  app.get("/orders", getOrdersController.handle.bind(getOrdersController));
+
+  app.get(
+    "/orders/:orderId",
+    getOrderByIdController.handle.bind(getOrdersController)
   );
 }
