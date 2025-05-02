1fb9a7156de9a7365388723386d5630b7b3afe7a
David-jos3 <davidgomesgc230@gmail.com>
2025-05-01 21:29:30 -0300

    feat(router): add routes for get, update, delete and get by id for menu items

--- src/http/controllers/menu/router.ts
+++ src/http/controllers/menu/router.ts
@@ -1,11 +1,38 @@
 import type { Application } from "express";
 import { CreateMenuItemController } from "./create-menu-item.controller";
+import { GetMenuItemController } from "./get-menu-item.controller";
+import { DeleteMenuItemController } from "./delete-menu-item.controller";
+import { GetMenuItemByIdController } from "./get-menu-item-by-id.controller";
 
 export function menuItemRouter(app: Application) {
   const createMenuItemController = new CreateMenuItemController();
+  const getMenuItemController = new GetMenuItemController();
+  const deleteMenuItemController = new DeleteMenuItemController();
+  const updateMenuItemController = new CreateMenuItemController();
+  const getMenuItemByIdController = new GetMenuItemByIdController();
 
   app.post(
     "/menu-items",
-    createMenuItemController.create.bind(createMenuItemController)
+    createMenuItemController.handle.bind(createMenuItemController)
+  );
+
+  app.get(
+    "/menu-items",
+    getMenuItemController.handle.bind(getMenuItemController)
+  );
+
+  app.delete(
+    "/menu-items/:menuId",
+    deleteMenuItemController.handle.bind(deleteMenuItemController)
+  );
+
+  app.put(
+    "/menu-items/:menuId",
+    updateMenuItemController.handle.bind(updateMenuItemController)
+  );
+
+  app.get(
+    "/menu-items/:menuId",
+    getMenuItemByIdController.handle.bind(getMenuItemByIdController)
   );
 }
