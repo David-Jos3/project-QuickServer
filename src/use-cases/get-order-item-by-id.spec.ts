a83ed7051be90e01614cc86603cce0555d6ce9a8
David-jos3 <davidgomesgc230@gmail.com>
2025-05-02 09:22:48 -0300

    test: add GetOrderItemByIdUseCase unit test

---
+++ src/use-cases/get-order-item-by-id.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryOrdersItemsRepository } from "../../test/repositories/in-memory-orders-items-repository";
+import { CreateOrdersItemsUseCase } from "./create-orders-items";
+import { GetOrderItemByIdUseCase } from "./get-order-item-by-id";
+
+let inMemoryOrdersItemsRepository: InMemoryOrdersItemsRepository;
+let getOrderItemByIdUseCase: GetOrderItemByIdUseCase;
+let createOrdersItemsUseCase: CreateOrdersItemsUseCase;
+
+describe("Get order Item By Id", () => {
+  beforeEach(() => {
+    inMemoryOrdersItemsRepository = new InMemoryOrdersItemsRepository();
+    createOrdersItemsUseCase = new CreateOrdersItemsUseCase(
+      inMemoryOrdersItemsRepository
+    );
+    getOrderItemByIdUseCase = new GetOrderItemByIdUseCase(
+      inMemoryOrdersItemsRepository
+    );
+  });
+
+  test("should be able to get a order item by id", async () => {
+    const { orderItem } = await createOrdersItemsUseCase.execute({
+      itemId: "item-fake-id",
+      orderId: "order-fake-id",
+      quantity: 2,
+    });
+
+    const result = await getOrderItemByIdUseCase.execute({
+      orderId: orderItem.id,
+    });
+
+    expect(result.orderItem).toEqual(orderItem);
+    expect(result.orderItem.id).toEqual(orderItem.id);
+  });
+});
