de282f212bf8fa548eb2a211a8ed130d213fce0f
David-jos3 <davidgomesgc230@gmail.com>
2025-05-01 21:37:45 -0300

    feat(router): expand orders-items routes with list and get by ID functionality

--- src/http/controllers/orders-items/router.ts
+++ src/http/controllers/orders-items/router.ts
@@ -1,10 +1,25 @@
 import type { Application } from "express";
 import { CreateOrdersItemsController } from "./create-orders-items.controller";
+import { ListOrderItemsController } from "./list-order-items.controller";
+import { GetOrderItemsByIdController } from "./get-order-items-by-id.controller";
 
 export function ordersItemsRouter(app: Application) {
   const createOrdersItemsController = new CreateOrdersItemsController();
+  const listOrdersItemsController = new ListOrderItemsController();
+  const getOrderItemsByIdController = new GetOrderItemsByIdController();
+
   app.post(
     "/orders-items",
-    createOrdersItemsController.create.bind(createOrdersItemsController)
+    createOrdersItemsController.handle.bind(createOrdersItemsController)
+  );
+
+  app.get(
+    "/orders-items",
+    listOrdersItemsController.handle.bind(listOrdersItemsController)
+  );
+
+  app.get(
+    "/orders-items/:orderId",
+    getOrderItemsByIdController.handle.bind(getOrderItemsByIdController)
   );
 }
