4f1749273a409f473e39f054ce4cbbb25eeaede3
David-jos3 <davidgomesgc230@gmail.com>
2025-05-14 22:32:35 -0300

    feat(env): add NODE_ENV and JWT keys validation

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,15 +1,20 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-	PORT: z.coerce.number().optional().default(3333),
+  PORT: z.coerce.number().optional().default(3333),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-	console.error("Invalid environment variables", _env.error.format());
+  console.error("Invalid environment variables", _env.error.format());
 
-	throw new Error("Invalid environment variables");
+  throw new Error("Invalid environment variables");
 }
 
 export const env = _env.data;
