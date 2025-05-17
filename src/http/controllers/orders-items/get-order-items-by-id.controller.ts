f5cfb63d325173145fd9cdb6c2be85edee34a027
David-jos3 <davidgomesgc230@gmail.com>
2025-05-16 23:24:03 -0300

    refactor(controller): rename param from orderId to orderItemId in GetOrderItemsByIdController

--- src/http/controllers/orders-items/get-order-items-by-id.controller.ts
+++ src/http/controllers/orders-items/get-order-items-by-id.controller.ts
@@ -4,19 +4,19 @@ import { PrismaOrdersItemsRepository } from "@/repositories/prisma/prisma-orders
 import { GetOrderItemByIdUseCase } from "@/use-cases/get-order-item-by-id";
 
 const getOrderItemsByIdBodySchema = z.object({
-  orderId: z.string().uuid(),
+  orderItemId: z.string().uuid(),
 });
 
 export class GetOrderItemsByIdController {
   async handle(request: Request, response: Response): Promise<void> {
     try {
-      const { orderId } = getOrderItemsByIdBodySchema.parse(request.params);
+      const { orderItemId } = getOrderItemsByIdBodySchema.parse(request.params);
       const prismaOrdersItemsRepository = new PrismaOrdersItemsRepository();
       const getOrderItemByIdUseCase = new GetOrderItemByIdUseCase(
         prismaOrdersItemsRepository
       );
 
-      const orderItem = await getOrderItemByIdUseCase.execute({ orderId });
+      const orderItem = await getOrderItemByIdUseCase.execute({ orderItemId });
 
       response.status(200).send({ orderItem });
     } catch (error) {
