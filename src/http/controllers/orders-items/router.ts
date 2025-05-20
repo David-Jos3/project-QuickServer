396c90ec667fec8b0cfa337e2cdcb830a21e46d5
David-jos3 <davidgomesgc230@gmail.com>
2025-05-19 21:47:23 -0300

    feat: add DELETE route to remove order item and update POST path

--- src/http/controllers/orders-items/router.ts
+++ src/http/controllers/orders-items/router.ts
@@ -2,14 +2,16 @@ import type { Application } from "express";
 import { CreateOrdersItemsController } from "./create-orders-items.controller";
 import { ListOrderItemsController } from "./list-order-items.controller";
 import { GetOrderItemsByIdController } from "./get-order-items-by-id.controller";
+import { RemoveItemFromOrderController } from "./remove-item-from-order.controller";
 
 export function ordersItemsRouter(app: Application) {
   const createOrdersItemsController = new CreateOrdersItemsController();
   const listOrdersItemsController = new ListOrderItemsController();
   const getOrderItemsByIdController = new GetOrderItemsByIdController();
+  const removeItemFromOrderController = new RemoveItemFromOrderController();
 
   app.post(
-    "/orders-items",
+    "/orders-item",
     createOrdersItemsController.handle.bind(createOrdersItemsController)
   );
 
@@ -19,7 +21,12 @@ export function ordersItemsRouter(app: Application) {
   );
 
   app.get(
-    "/orders-items/:orderId",
+    "/orders-items/:orderItemId",
     getOrderItemsByIdController.handle.bind(getOrderItemsByIdController)
+  );
+
+  app.delete(
+    "/orders/items/:itemId",
+    removeItemFromOrderController.handle.bind(removeItemFromOrderController)
   );
 }
