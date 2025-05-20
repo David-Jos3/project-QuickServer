d117a0952113025a50ece1169f081e0c7771f32a
David-jos3 <davidgomesgc230@gmail.com>
2025-05-19 21:53:46 -0300

    feat(router): add login route and AuthLoginController to userRouter

--- src/http/controllers/users/router.ts
+++ src/http/controllers/users/router.ts
@@ -1,10 +1,14 @@
+import { AuthLoginController } from "./auth-login.controller";
 import type { Application } from "express";
 import { RegisterUserController } from "./register-user.controller";
 
 export function userRouter(app: Application) {
   const registerUserController = new RegisterUserController();
+  const authLoginController = new AuthLoginController();
   app.post(
     "/users",
     registerUserController.handle.bind(registerUserController)
   );
+
+  app.post("/login", authLoginController.handle.bind(authLoginController));
 }
