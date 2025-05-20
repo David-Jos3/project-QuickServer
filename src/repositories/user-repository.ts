82dd0d7f40dab6aaf1cba1a7513b0a5a88be8717
David-jos3 <davidgomesgc230@gmail.com>
2025-05-19 21:53:17 -0300

    feat(users-repository): add findByUserName and findByEmail methods to UsersRepository interface

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -2,4 +2,6 @@ import type { Prisma, Users } from "@prisma/client";
 
 export interface UsersRepository {
   create(user: Prisma.UsersCreateInput): Promise<Users>;
+  findByUserName(username: string): Promise<Users | null>;
+  findByEmail(email: string): Promise<Users | null>;
 }
