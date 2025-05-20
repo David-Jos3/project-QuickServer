7e1f2f4d33ceba1f7a18113497a3471717e506ea
David-jos3 <davidgomesgc230@gmail.com>
2025-05-19 21:51:11 -0300

    feat(user): add username field and validations for email and username uniqueness in RegisterUserUseCase; hash password before saving user

--- src/use-cases/register-user.ts
+++ src/use-cases/register-user.ts
@@ -1,8 +1,11 @@
 import type { UserRoles, Users } from "@prisma/client";
+import bcrypt from "bcrypt";
 import type { UsersRepository } from "../repositories/user-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credential-error";
 
 interface RegisterUserUseCaseRequest {
   name: string;
+  username: string;
   email: string;
   password: string;
   userRoles: UserRoles;
@@ -17,14 +20,30 @@ export class RegisterUserUseCase {
 
   async execute({
     name,
+    username,
     email,
     password,
     userRoles,
   }: RegisterUserUseCaseRequest): Promise<RegisterUserUseCaseResponse> {
+    const emailExist = await this.usersRepository.findByEmail(email);
+
+    if (emailExist) {
+      throw new InvalidCredentialsError();
+    }
+
+    const userNameExist = await this.usersRepository.findByUserName(username);
+
+    if (userNameExist) {
+      throw new InvalidCredentialsError();
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
     const user = await this.usersRepository.create({
       name,
+      username,
       email,
-      password,
+      password: hashedPassword,
       role: userRoles,
     });
 
