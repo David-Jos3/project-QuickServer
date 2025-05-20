0d054a5c67422b8620c6ece55009764c5522f24a
David-jos3 <davidgomesgc230@gmail.com>
2025-05-19 21:13:23 -0300

    docs: integrate Swagger UI to serve API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 import { userRouter } from "./http/controllers/users/router";
 import { menuItemRouter } from "./http/controllers/menu/router";
 import { ordersRouter } from "./http/controllers/orders/router";
 import { ordersItemsRouter } from "./http/controllers/orders-items/router";
+import swaggerDocument from "./docs/swagger.json";
 
 export const app = express();
 
@@ -11,6 +14,8 @@ userRouter(app);
 menuItemRouter(app);
 ordersRouter(app);
 ordersItemsRouter(app);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/sobre", (req, res) => {
   res.send({ message: "Sistema de Pedidos Missão Lancai as Redes" });
