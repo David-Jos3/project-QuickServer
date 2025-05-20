56fabfbeed215b7a7d374fc78011dae85304a153
David-jos3 <davidgomesgc230@gmail.com>
2025-05-19 21:52:14 -0300

    feat(user-repository): add findByUserName and findByEmail methods to PrismaUserRepository

--- src/repositories/prisma/prisma-user-repository.ts
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -3,6 +3,20 @@ import type { UsersRepository } from "../user-repository";
 import { prisma } from "../../lib/prisma-client";
 
 export class PrismaUserRepository implements UsersRepository {
+  async findByUserName(username: string): Promise<Users | null> {
+    const user = await prisma.users.findUnique({
+      where: { username: username },
+    });
+
+    return user ?? null;
+  }
+  async findByEmail(email: string): Promise<Users | null> {
+    const user = await prisma.users.findUnique({
+      where: { email: email },
+    });
+
+    return user ?? null;
+  }
   async create(data: Users): Promise<Users> {
     return await prisma.users.create({
       data,
