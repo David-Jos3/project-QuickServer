0d20a629001b912c960b1d96065f8836a1611b96
David-jos3 <davidgomesgc230@gmail.com>
2025-05-23 21:35:44 -0300

    feat(users-repository): add findById and updatePassword methods to UsersRepository interface

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -4,4 +4,6 @@ export interface UsersRepository {
   create(user: Prisma.UsersCreateInput): Promise<Users>;
   findByUserName(username: string): Promise<Users | null>;
   findByEmail(email: string): Promise<Users | null>;
+  findById(userId: string): Promise<Users | null>;
+  updatePassword(user: string, password: string): Promise<void>;
 }
